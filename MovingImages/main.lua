c7c2e97cf22b46dcac2b72eea8c64d01126c3ea1
Jake Holtzhauer <47784147+yungprod@users.noreply.github.com>
2019-02-26 09:38:52 -0500

    Update main.lua

--- MovingImages/main.lua
+++ MovingImages/main.lua
@@ -14,52 +14,52 @@ scrollspeed = 3
 local backrgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
 
 -- character image with width and height
-local beetleship = display.newImageRect ("Images/beetleship.png", 200, 200)
+local beetleship = display.newImageRect("Images/beetleship.png", 200, 200)
 
 -- set the image to be transparent
-beetleship	.alpha = 0
+beetleship.alpha = 0
 
 --set the initial x and y position of beetleship
 beetleship.x = 0
 beetleship.y = display.contentHeight/3
 
---function: MoveShip
+-- function: MoveShip
 -- input: this function accepts and event listener
---Output: None
+-- Output: None
 -- description: This function adds the scroll speed to the x-value of the ship
 local function MoveShip(event)
 	-- add the scroll speed to the x-value of the ship
 beetleship.x = beetleship.x + scrollspeed	
--- change the transperency of the shit every time it moves so that it fades out
-beetleship.alpha = beetleship.alpha + 00.1
+-- change the transperency of the ship every time it moves so that it fades out
+beetleship.alpha = beetleship.alpha + 0.01
 end
 
--- Moveship will be calle over and over again
+-- Moveship will be called over and over again
 Runtime:addEventListener("enterFrame", MoveShip)
 
-
--- backround imag with width and height
-local backrgroundImage = display.newImageRect("Images/background.png", 2048, 1536)
+---------------------------------------------------------------------
+--global variables
+local scrollspeed = 5
 
 -- character image with width and height
-local octopus = display.newImageRect ("Images/octopus.png", 200, 200)
+local octopus = display.newImageRect ("Images/octopus.png", 800, 800)
 
 -- set the image to be transparent
 octopus.alpha = 0
 
 --set the initial x and y position of beetleship
-octopus.x = 0
+octopus.x = 1024
 octopus.y = display.contentHeight/3
 
---function: MoveShip
+-- function: MoveShip
 -- input: this function accepts and event listener
---Output: None
+-- Output: None
 -- description: This function adds the scroll speed to the x-value of the ship
 local function MoveOctopus(event)
 	-- add the scroll speed to the x-value of the ship
 octopus.x = octopus.x + scrollspeed	
--- change the transperency of the shit every time it moves so that it fades out
-octopus.alpha = beetleship.alpha + 00.1
+-- change the transperency of the octopus every time it moves so that it fades out
+octopus.alpha = octopus.alpha + 00.1
 end
 
 -- Moveship will be called over and over again
