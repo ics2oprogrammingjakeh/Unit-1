f7f96b13e82092e12b17d9c328b9747983d12ccc
Jake Holtzhauer <47784147+yungprod@users.noreply.github.com>
2019-04-05 08:51:52 -0400

    Update main.lua

--- WhackAMoleJakeH2/main.lua
+++ WhackAMoleJakeH2/main.lua
@@ -1,7 +1,9 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+--Title: WhackAMole
+-- Name: Jake Holtzhauer
+-- Course: ICS2O/3C
+-- This program places a random object on the screen. if the user clicks on it in time, 
+--the score increases by 1
 
--- Your code here
+display.setStatusBar(display.HiddenStatusBar)
+
+local bkg = display.newRect(o, o, display.contentWidth, display.contentHeight)
